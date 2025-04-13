bcd22b4bf19373682e3f4eb81e25a80d9c567cbf
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-04-13 12:15:54 -0400

    restyle graphs and pages

--- viz3.js
+++ viz3.js
@@ -1,21 +1,19 @@
 const stroke = d3.csv("stroke-data.csv");
 
 stroke.then(function (data) {
-
-  // filter data to only include people above 16 years old (like the graph)
   data = data.filter(d => +d.age > 16);
 
   data.forEach(function (d) {
     d.avg_glucose_level = +d.avg_glucose_level;
   });
 
-  let width = 800, height = 800;
+  let width = 672, height = 672;
 
   let margin = {
-    top: 30,
-    bottom: 50,
-    right: 30,
-    left: 50
+    top: 25.2,
+    bottom: 42,
+    right: 25.2,
+    left: 42
   }
 
   let svg = d3.select("#boxplot")
@@ -44,15 +42,17 @@ stroke.then(function (data) {
     .attr('transform', `translate(0, ${height - margin.bottom})`)
 
   svg.append('text')
-    .attr('x', width / 2 - 40)
-    .attr('y', height - 15)
+    .attr('x', width / 2 - 33.6)
+    .attr('y', height - 12.6)
     .text('Stroke Status')
+    .attr('font-size', '12.6px')
 
   svg.append('text')
-    .attr('x', -500)
-    .attr('y', 10)
+    .attr('x', -420)
+    .attr('y', 8.4)
     .text('Average Glucose Level (mg/dL)')
     .attr('transform', 'rotate(-90)')
+    .attr('font-size', '12.6px')
 
   const rollupFunction = function (groupData) {
     const values = groupData.map(d => d.avg_glucose_level).sort(d3.ascending);
@@ -76,7 +76,7 @@ stroke.then(function (data) {
       .attr("y1", yscale(quantiles.max))
       .attr("y2", yscale(quantiles.min))
       .attr("stroke", "black")
-      .style("stroke-width", 2);
+      .style("stroke-width", 1.68);
 
     svg.append("rect")
       .attr("x", x)
@@ -92,82 +92,77 @@ stroke.then(function (data) {
       .attr("y1", yscale(quantiles.median))
       .attr("y2", yscale(quantiles.median))
       .attr("stroke", "black")
-      .style("width", 20);
+      .style("width", 16.8);
 
     svg.append("line")
-      .attr("x1", 50)
-      .attr("x2", 770)
+      .attr("x1", 42)
+      .attr("x2", 646.8)
       .attr("y1", yscale(100))
       .attr("y2", yscale(100))
       .attr("stroke", "green")
-      .style("width", 20);
+      .style("width", 16.8);
 
     svg.append("line")
-      .attr("x1", 50)
-      .attr("x2", 770)
+      .attr("x1", 42)
+      .attr("x2", 646.8)
       .attr("y1", yscale(70))
       .attr("y2", yscale(70))
       .attr("stroke", "green")
-      .style("width", 20);
+      .style("width", 16.8);
 
     svg.append("line")
-      .attr("x1", 50)
-      .attr("x2", 50)
+      .attr("x1", 42)
+      .attr("x2", 42)
       .attr("y1", yscale(70))
       .attr("y2", yscale(100))
       .attr("stroke", "green")
-      .style("width", 20);
+      .style("width", 16.8);
 
     svg.append("line")
-      .attr("x1", 770)
-      .attr("x2", 770)
+      .attr("x1", 646.8)
+      .attr("x2", 646.8)
       .attr("y1", yscale(70))
       .attr("y2", yscale(100))
       .attr("stroke", "green")
-      .style("width", 20);
+      .style("width", 16.8);
 
     svg.append('text')
-      .attr('x', width / 2 - 203)
-      .attr('y', height - 145)
+      .attr('x', width / 2 - 170.1)
+      .attr('y', height - 121.8)
       .text('Typical Glucose Level Range for Healthy People over the age of 16')
       .attr("stroke", "blue")
-      .attr("font-size", 15)
+      .attr("font-size", 12.6)
       .attr("font-weight", 200)
 
     svg.append("rect")
-      .attr("x", 50)
-      .attr("y", 600)
-      .attr("height", 100)
-      .attr("width", 770 - 50)
+      .attr("x", 42)
+      .attr("y", 504)
+      .attr("height", 84)
+      .attr("width", 604.8)
       .attr("stroke", "green")
       .style("fill", "green")
       .style("opacity", 0.2)
 
     svg.append("rect")
-      .attr("x", 50)
-      .attr("y", 758)
-      .attr("height", 10)
-      .attr("width", 770 - 50)
+      .attr("x", 42)
+      .attr("y", 636.72)
+      .attr("height", 8.4)
+      .attr("width", 604.8)
       .attr("stroke", "white")
       .style("fill", "white")
   });
 
   svg.append('text')
-    .attr('x', width / 2 - 230)
-    .attr('y', 0)
-    .text('Average Glucose Level by Stroke Status for People over the age of 16')
-
-  svg.append('text')
-    .attr('x', width / 2 + 123)
+    .attr('x', width / 2 + 103.32)
     .attr('y', yscale(50))
     .text('No Stroke')
-    .attr("font-size", 12)
+    .attr("font-size", 10.08)
     .attr("font-weight", 400)
 
   svg.append('text')
-    .attr('x', 233)
+    .attr('x', 195.72)
     .attr('y', yscale(50))
     .text('Had Stroke')
-    .attr("font-size", 12)
+    .attr("font-size", 10.08)
     .attr("font-weight", 400)
 });
